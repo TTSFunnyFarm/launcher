067b7d3026a36ca3c94089118de98d689ad4945a
John Cote <jwcotejr@users.noreply.github.com>
2019-06-17 05:51:32 -0400

    ui: add update frame to launcher

--- src/ui/LauncherWindow.cpp
+++ src/ui/LauncherWindow.cpp
@@ -19,6 +19,9 @@ LauncherWindow::LauncherWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    // Update frame not visible by default:
+    ui->frame_update->setVisible(false);
+
 #ifdef Q_OS_WIN
     setTitleBar(ui->launcher_window);
     addIgnoreWidget(ui->frame_toon_news);
@@ -49,7 +52,12 @@ void LauncherWindow::on_push_button_minimize_clicked()
 
 void LauncherWindow::on_push_button_play_clicked()
 {
-    // TODO
+    // Disable the settings button:
+    ui->push_button_settings->setEnabled(false);
+
+    // Hide play button, show update frame:
+    ui->push_button_play->setVisible(false);
+    ui->frame_update->setVisible(true);
 }
 
 void LauncherWindow::on_push_button_website_clicked()
